19da31ddc6b2cd73740a75b6083d51a5fd24cc23
Mark <markrassamni@me.com>
2018-10-01 17:55:47 -0700

    Initial Commit

---
+++ Assignment3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Assignment3
+//
+//  Created by Mark Rassamni on 10/1/18.
+//  Copyright © 2018 Mark Rassamni. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
