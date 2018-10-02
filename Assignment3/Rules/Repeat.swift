c1ba1d028e08f48f9337f965497e10cb1aaced71
Mark <markrassamni@me.com>
2018-10-02 00:56:05 -0700

    repeat rule

---
+++ Assignment3/Rules/Repeat.swift
@@ -0,0 +1,25 @@
+//
+//  Repeat.swift
+//  Assignment3
+//
+//  Created by Mark Rassamni on 10/1/18.
+//  Copyright © 2018 Mark Rassamni. All rights reserved.
+//
+
+import Foundation
+
+class Repeat {
+    
+    /*
+     repeat k
+     statement1
+     statement2
+     ...
+     statementJ
+     end
+ 
+ */
+    
+    // k is an integer. The statements inside the repeat block are any legal statement in the language. the repeat statement has the obvious semantics.
+    
+}
