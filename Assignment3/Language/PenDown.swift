d5c1bccd4f0953ef6ec4feb46a3f2a406300e3d6
Mark <markrassamni@me.com>
2018-10-13 15:25:47 -0700

    remove extra functions

--- Assignment3/Language/PenDown.swift
+++ Assignment3/Language/PenDown.swift
@@ -9,21 +9,7 @@
 import Foundation
 
 class PenDown: Statement {
-
-//    When the pen is down and the turtle moves a black line is drawn
-    
-    /*
-    func evaluate(values: [String : Expression]) -> Location {
-        return Location(x: 0.0, y: 0.0, position: (x: 0.0, y: 0.0), rotation: 0.0)
-    }
- */
-    func evaluate(values: Context) {
-        
-    }
-    
-    
     var description: String {
         return "Pen placed down."
     }
-    
 }
--- Assignment3/Language/PenUp.swift
+++ Assignment3/Language/PenUp.swift
@@ -8,30 +8,8 @@
 
 import Foundation
 
-class PenUp: Statement { //TODO: Inherit statement - check all classes
-    
-    // When the pen is up and the turtle moves nothing is drawn.
-    
-    /*
-    func evaluate(values: [String : Expression]) -> Location {
-        return Location(x: 0.0, y: 0.0, position: (x: 0.0, y: 0.0), rotation: 0.0)
-    }
- */
-    func evaluate(values: Context) {
-        
-    }
-    
-    
+class PenUp: Statement {
     var description: String {
         return "Pen picked up."
     }
-    
-//    func evaluate(values: Context) -> Bool {
-//        return true
-//    }
-    
-    
-    
-    
-    
 }
