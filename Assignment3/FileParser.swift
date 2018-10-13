4ba3242438a86761a3be876d3b20ac3225873ae6
Mark <markrassamni@me.com>
2018-10-13 13:48:47 -0700

    fix bug when creating repeat statements. Can successfully create a program now

--- Assignment3/FileParser.swift
+++ Assignment3/FileParser.swift
@@ -40,20 +40,24 @@ class FileParser {
         var currentLine = 0
         var statements = [Statement]()
         
-        // Have to do lines.count-1 because all files created in xcode (used to create .txt files) have an empty line at the end of file. Trying to read the empty line will return nil
-        while currentLine < lines.count - 1 {
+        while currentLine < lines.count {
             let line = lines[currentLine] // combine this and current expr... add .comp sep by here, dont put .first here, switch line.first, refactor inside cases to use line instead of guarding stuff
-            guard var currentStatement = line.components(separatedBy: " ").first else { return nil }
-            guard let firstChar = currentStatement.first else { return nil }
-            if String(firstChar).lowercased() == assignment.lowercased() {
-                currentStatement = assignment
+            let fullStatement = line.components(separatedBy: " ").filter{ $0 != "" }
+            guard var statementType = fullStatement.first else {
+                // This allows skipping blank lines. Necessary because when using .txt files in xcode (or any file) it will automatically add an empty line to the last line of the file that cannot be removed.
+                currentLine += 1
+                continue
             }
-            currentStatement = currentStatement.lowercased()
-            switch currentStatement {
+            guard let firstChar = statementType.first else { return nil }
+            if String(firstChar).lowercased() == assignment.lowercased() {
+                statementType = assignment
+            }
+            statementType = statementType.lowercased()
+            switch statementType {
             case moveCommand.lowercased():
                 currentLine += 1
-                guard line.components(separatedBy: " ").count == 2 else { return nil }
-                let value = line.components(separatedBy: " ")[1]
+                guard fullStatement.count == 2 else { return nil }
+                let value = fullStatement[1]
                 if let distance = Int(value){
                     statements.append(Move(distance: distance))
                 } else {
@@ -67,20 +71,20 @@ class FileParser {
                 statements.append(PenDown())
             case turnCommand.lowercased():
                 currentLine += 1
-                guard line.components(separatedBy: " ").count == 2 else { return nil }
-                let value = line.components(separatedBy: " ")[1]
+                guard fullStatement.count == 2 else { return nil }
+                let value = fullStatement[1]
                 if let degrees = Int(value){
                     statements.append(Turn(degrees: degrees))
                 } else {
                     statements.append(Turn(variableName: value))
                 }
             case repeatCommand.lowercased():
-                guard line.components(separatedBy: " ").count == 2 else { return nil }
-                let repeatCount = line.components(separatedBy: " ")[1]
+                guard fullStatement.count == 2 else { return nil }
+                let repeatCount = fullStatement[1]
                 let optionalEndIndex = lines[currentLine..<lines.count].enumerated().filter {$0.element.lowercased() == endRepeat.lowercased()}.map{$0.offset}.first
                 guard let endIndex = optionalEndIndex else { return nil }
-                currentLine = endIndex + 1
                 let repeatedLines = lines.enumerated().filter{$0.offset > currentLine && $0.offset < endIndex }.map{$0.element}
+                currentLine = endIndex + 1
                 guard let repeatedStatements = convertToStatements(lines: repeatedLines) else { return nil }
                 if let count = Int(repeatCount){
                     statements.append(Repeat(statements: repeatedStatements, repeatCount: count))
