dc0d40b67a0f5ce9aaf84171fc8d47afd569c9f3
Mark <markrassamni@me.com>
2018-10-13 14:32:02 -0700

    test parsing does not return nil

--- Assignment3Tests/Assignment3Tests.swift
+++ Assignment3Tests/Assignment3Tests.swift
@@ -25,49 +25,16 @@ class Assignment3Tests: XCTestCase {
     }
     
     func testParsing(){
+        // TODO: Make statements equatable
         let statements = fileParser.parse(file: "test1.txt")
         XCTAssertNotNil(statements)
         let expectedStatements: [Statement] = [Repeat(statements: [Move(distance: 10), Move(distance: 15), Move(distance: 20)], repeatCount: 2), Move(distance: 10), Turn(degrees: 10), Move(distance: 10)]
-//        XCTAssertEqual(statements!, expectedStatements)
-        //        XCTAssertEqual(statements!, expectedStatements)
-//        XCTAssertEqual(statements!, expectedStatements)
         /*
-         Repeat 2
-         move 10
-         mOve 15
-         move 20
-         end
-         move 10
-         turn 10
-         move 10
-         
-        */
-    }
-    
-    /*
-    func testParsing(){
-        guard let fileParser = FileParser(file: "test1.txt") else {
-            XCTAssertTrue(false)
-            return
+        XCTAssertEqual(statements?.count, expectedStatements.count)
+        for (index, statement) in expectedStatements.enumerated() {
+            XCTAssertEqual(statement, statements![index])
         }
-        for _ in 0..<fileParser.lines.count {
-            let expression = fileParser.parseNextCommand()
-            print(expression ?? "Not returned")
-        }
-        
-    }
-     */
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+         */
     }
 
 }
