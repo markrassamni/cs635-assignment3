b47bcb6e486c93586721f5f894d0462821f0bd2a
Mark <markrassamni@me.com>
2018-10-13 16:55:04 -0700

    create visitor protocol

---
+++ Assignment3/Visitors/Visitor.swift
@@ -0,0 +1,21 @@
+//
+//  Visitor.swift
+//  Assignment3
+//
+//  Created by Mark Rassamni on 10/13/18.
+//  Copyright © 2018 Mark Rassamni. All rights reserved.
+//
+
+import Foundation
+
+protocol Visitor {
+    var turtle: Turtle { get }
+    var context: Context { get }
+    func visit(_ program: Program)
+    func visit(_ penUp: PenUp)
+    func visit(_ penDown: PenDown)
+    func visit(_ move: Move)
+    func visit(_ turn: Turn)
+    func visit(_ repeat: Repeat)
+    func visit(_ assignment: Assignment)
+}
