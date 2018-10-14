32ce3baeb664bd88ca4488905121537c99d237e5
Mark <markrassamni@me.com>
2018-10-14 01:39:37 -0700

    execute func for program

--- Assignment3/Program.swift
+++ Assignment3/Program.swift
@@ -22,16 +22,42 @@ class Program{
         visitor.visit(self)
     }
     
-    // TODO: Need to finish this or remove? if not also remove turtle class var
     // Need to execute without visitor. If we have visitor and we call execute then visitor does its stuff. Above call to accept visitor should not also execute
+    // TODO: Should change this to expressions and call expression.eval? How to handle pen? Remove extra class vars when not needed
     func execute(){
         for statement in statements{
-            switch statement{
-            case is PenUp:
-                turtle.penUp()
-            default:
-                return
+            evaluate(statement: statement)
+        }
+    }
+    
+    private func evaluate(statement: Statement){
+        switch statement{
+        case is PenUp:
+            turtle.penUp()
+        case is PenDown:
+            turtle.penDown()
+        case is Move:
+            guard let move = statement as? Move else { return }
+            guard let value = move.interpret(context: context) else { return }
+            turtle.move(distance: value)
+        case is Turn:
+            guard let turn = statement as? Turn else { return }
+            guard let value = turn.interpret(context: context) else { return }
+            turtle.turn(degrees: value)
+        case is Repeat:
+            guard let repeatStatement = statement as? Repeat else { return }
+            guard let count = repeatStatement.interpret(context: context) else { return }
+            for _ in 0..<count {
+                for statement in repeatStatement.statements {
+                    evaluate(statement: statement)
+                }
             }
+        case is Assignment:
+            guard let assignment = statement as? Assignment else { return }
+            let variable: (name: String, value: Int) = assignment.variable
+            context.setValue(for: variable.name, to: variable.value)
+        default:
+            return
         }
     }
 }
