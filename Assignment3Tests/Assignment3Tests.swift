8e57e27dbc92c97d653546f03553d9e33c765dc2
Mark <markrassamni@me.com>
2018-10-13 23:22:11 -0700

    reimplement variable caretaker test

--- Assignment3Tests/Assignment3Tests.swift
+++ Assignment3Tests/Assignment3Tests.swift
@@ -30,23 +30,6 @@ class Assignment3Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testParsing(){
-        // TODO: Make statements equatable
-        let ast = fileParser.buildProgram(fromFile: "test1.txt")
-        XCTAssertNotNil(ast)
-        let expectedStatements: [Statement] = [PenDown(), Repeat(statements: [Move(distance: 10)!, Move(distance: 15)!, Move(distance: 20)!], repeatCount: 2), Move(distance: 10)!, Turn(degrees: 10), Move(distance: 10)!]
-        let expectedAST = Program()
-        for statement in expectedStatements {
-            expectedAST.add(statement: statement)
-        }
-        /*
-        XCTAssertEqual(statements?.count, expectedStatements.count)
-        for (index, statement) in expectedStatements.enumerated() {
-            XCTAssertEqual(statement, statements![index])
-        }
-         */
-    }
-    
     func testTurtleSquare(){
         let turtle = Turtle()
         turtle.penDown()
@@ -75,7 +58,7 @@ class Assignment3Tests: XCTestCase {
         XCTAssertEqual(turtle.currentDirection, 360)
     }
     
-    func testTurtle2(){
+    func testTurtleMoveAndRotate(){
         let turtle = Turtle()
         turtle.penDown()
         XCTAssertEqual(turtle.location().x, 0.0, accuracy: 0.001)
@@ -108,7 +91,6 @@ class Assignment3Tests: XCTestCase {
         memento.setState(name: turtle.locationState, value: Point(0,0))
         memento.setState(name: turtle.directionState, value: 0)
         memento.setState(name: turtle.penDownState, value: false)
-        
         XCTAssertEqual(mementos[0], memento)
         memento.setState(name: turtle.penDownState, value: true)
         XCTAssertEqual(mementos[1], memento)
@@ -130,38 +112,34 @@ class Assignment3Tests: XCTestCase {
         XCTAssertEqual(mementos[9], memento)
         memento.setState(name: turtle.locationState, value: Point(109.84808,  1.73648))
         XCTAssertEqual(mementos[10], memento)
-        
-        
-        
-//        XCTAssertEqual(mementos[0], Memento(direction: 0, location: Point(0,0), isPenDown: false))
-//        XCTAssertEqual(mementos[1], Memento(direction: 0, location: Point(0,0), isPenDown: true))
-//        XCTAssertEqual(mementos[2], Memento(direction: 0, location: Point(10,0), isPenDown: true))
-//        XCTAssertEqual(mementos[3], Memento(direction: 0, location: Point(25,0), isPenDown: true))
-//        XCTAssertEqual(mementos[4], Memento(direction: 0, location: Point(45,0), isPenDown: true))
-//        XCTAssertEqual(mementos[5], Memento(direction: 0, location: Point(55,0), isPenDown: true))
-//        XCTAssertEqual(mementos[6], Memento(direction: 0, location: Point(70,0), isPenDown: true))
-//        XCTAssertEqual(mementos[7], Memento(direction: 0, location: Point(90,0), isPenDown: true))
-//        XCTAssertEqual(mementos[8], Memento(direction: 0, location: Point(100,0), isPenDown: true))
-//        XCTAssertEqual(mementos[9], Memento(direction: 10, location: Point(100,0), isPenDown: true))
-//        XCTAssertEqual(mementos[10], Memento(direction: 10, location: Point(109.84808,  1.73648), isPenDown: true))
     }
     
-    /*
     func testCaretakerVisitorWithVariable(){
         let ast = fileParser.buildProgram(fromFile: "test3.txt")
         XCTAssertNotNil(ast)
         ast!.accept(visitor: caretakerVisitor)
         let mementos = caretakerVisitor.mementos
         XCTAssertEqual(mementos.count, 9)
-        XCTAssertEqual(mementos[0], Memento(direction: 0, location: Point(0,0), isPenDown: false))
-        XCTAssertEqual(mementos[1], Memento(direction: 0, location: Point(0,0), isPenDown: true))
-        XCTAssertEqual(mementos[2], Memento(direction: 0, location: Point(15,0), isPenDown: true))
-        XCTAssertEqual(mementos[3], Memento(direction: 90, location: Point(15,0), isPenDown: true))
-        XCTAssertEqual(mementos[4], Memento(direction: 90, location: Point(15,15), isPenDown: true))
-        XCTAssertEqual(mementos[5], Memento(direction: 180, location: Point(15,15), isPenDown: true))
-        XCTAssertEqual(mementos[6], Memento(direction: 180, location: Point(0,15), isPenDown: true))
-        XCTAssertEqual(mementos[7], Memento(direction: 270, location: Point(0,15), isPenDown: true))
-        XCTAssertEqual(mementos[8], Memento(direction: 270, location: Point(0,0), isPenDown: true))
+        let memento = Memento()
+        memento.setState(name: turtle.locationState, value: Point(0,0))
+        memento.setState(name: turtle.directionState, value: 0)
+        memento.setState(name: turtle.penDownState, value: false)
+        XCTAssertEqual(mementos[0], memento)
+        memento.setState(name: turtle.penDownState, value: true)
+        XCTAssertEqual(mementos[1], memento)
+        memento.setState(name: turtle.locationState, value: Point(15,0))
+        XCTAssertEqual(mementos[2], memento)
+        memento.setState(name: turtle.directionState, value: 90)
+        XCTAssertEqual(mementos[3], memento)
+        memento.setState(name: turtle.locationState, value: Point(15,15))
+        XCTAssertEqual(mementos[4], memento)
+        memento.setState(name: turtle.directionState, value: 180)
+        XCTAssertEqual(mementos[5], memento)
+        memento.setState(name: turtle.locationState, value: Point(0,15))
+        XCTAssertEqual(mementos[6], memento)
+        memento.setState(name: turtle.directionState, value: 270)
+        XCTAssertEqual(mementos[7], memento)
+        memento.setState(name: turtle.locationState, value: Point(0,0))
+        XCTAssertEqual(mementos[8], memento)
     }
- */
 }
